5297f4f61972c7b3d65cc3f81cc2dabdd17908ad
harshieeh-source <harshieeh@gmail.com>
2026-07-21 23:21:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,25 @@
-import pandas as pd
-import numpy as np
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 
-data = pd.read_csv("lab1.csv")
+df = pd.read_csv('ecommerce_sales_analytics_5000.csv')
 
-data
+df.head()
 
-concepts = np.array(data)[:,:-1]
+df.tail()
 
-concepts
+df.shape
 
-target = np.array(data)[:,-1]
+df.columns
 
-target
-
-def train(con,tar):
-    for i,val in enumerate(tar):
-        if val=='yes':
-            specific_h = con[i].copy()
-            break
-
-    for i,val in enumerate(con):
-        if tar[i]=='yes':
-            for x in range(len(specific_h)):
-                if val[x] != specific_h[x]:
-                    specific_h[x] = '?'
-                else:
-                    pass
-    return specific_h      
-
-print(train(concepts,target))
-
+df.dtypes
